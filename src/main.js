a24c385062078da14f841ef5ce3fcc8d2026e320
peatroot <garethpeat@gmail.com>
2017-05-05 10:03:37 +0100

    Register VolcanoPlot and SamplePlot components

--- src/main.js
+++ src/main.js
@@ -5,12 +5,21 @@
 // 2. or, use next line to activate DEFAULT QUASAR STYLE
 require(`quasar/dist/quasar.${__THEME}.css`)
 // ==============================
+// Custom charts
+require(`../node_modules/volcano-plot/index.scss`)
+require(`../node_modules/sample-plot/index.scss`)
+// ==============================
 
 import Vue from 'vue'
 import Quasar from 'quasar'
 import router from './router'
+import VolcanoPlot from './components/VolcanoPlot.vue'
+import SamplePlot from './components/SamplePlot.vue'
 
 Vue.use(Quasar) // Install Quasar Framework
+
+Vue.component('volcano-plot', VolcanoPlot)
+Vue.component('sample-plot', SamplePlot)
 
 Quasar.start(() => {
   /* eslint-disable no-new */
