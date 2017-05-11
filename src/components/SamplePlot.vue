bb5f51209d738bef44f563b11e22a5fd2473058b
peatroot <garethpeat@gmail.com>
2017-05-11 16:38:35 +0100

    Add toggles for circle labels, regression, corrected IC50

--- src/components/SamplePlot.vue
+++ src/components/SamplePlot.vue
@@ -3,6 +3,18 @@
     <div class="card-title bg-primary text-white">Drug: {{ drugSummary.drugName }}, Transcription Factor: {{ tf }}</div>
     <div class="card-content">
       <div class="sample-plot"></div>
+      <label>
+        <q-checkbox v-model="showLabels"></q-checkbox>
+        Show Labels
+      </label>
+      <label>
+        <q-checkbox v-model="showRegression"></q-checkbox>
+        Show Regression
+      </label>
+      <label>
+        <q-checkbox v-model="useCorrectedIc50"></q-checkbox>
+        Use Corrected IC50
+      </label>
     </div>
   </div>
 </template>
@@ -17,6 +29,13 @@ export default {
   directives: {
     resize
   },
+  data () {
+    return {
+      showLabels: true,
+      showRegression: true,
+      useCorrectedIc50: true
+    }
+  },
   computed: {
     drugSummary: function () {
       let summary = store.getters.drugSummary(this.drug)
@@ -28,17 +47,21 @@ export default {
     this.createPlot()
   },
   updated () {
-    this.plot.data(store.getters.samplePlotData(this.drug, this.tf))
+    this.plot.data(store.getters.samplePlotData(this.drug, this.tf, this.useCorrectedIc50))
+             .showCircleLabels(this.showLabels)
+             .showRegression(this.showRegression)
+             .yLabel(this.useCorrectedIc50 ? 'Corrected IC50' : 'IC50')
              .render()
   },
   methods: {
     createPlot () {
       this.plot = volcanoPlot('.sample-plot')
-                    .data(store.getters.samplePlotData(this.drug, this.tf))
+                    .data(store.getters.samplePlotData(this.drug, this.tf, this.useCorrectedIc50))
                     .xAccessor(d => d.tfActivity)
                     .yAccessor(d => d.ic50)
+                    .textAccessor(d => d.sampleId)
                     .xLabel('Activity')
-                    .yLabel('IC50')
+                    .yLabel(this.useCorrectedIc50 ? 'Corrected IC50' : 'IC50')
       this.plot.render()
     },
     handleResize () {
