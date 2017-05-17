d0409c3f3806a6829716791a8763722646346174
peatroot <garethpeat@gmail.com>
2017-05-17 15:19:32 +0100

    Add possible layout

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -13,25 +13,75 @@
     -->
     <div class="layout-view">
       <div class="layout-padding">
-        <div class="card">
-          <div class="card-title bg-primary text-white">Drug Selection</div>
-          <div class="card-content">
-            <span>Please select a drug.</span>
-            <q-select type="list" v-model="selectedDrug" :options="drugs"></q-select>
+        
+        <!--start new layout-->
+        <!--<div class="column">
+          <div class="row lt-bg-column justify-center gutter">
+            <div class="bg-width-1of2 lg-width-2of5">
+              <div class="card">
+                <div class="card-title bg-primary text-white">Drug Selection</div>
+                <div class="card-content">
+                  <span>Please select a drug.</span>
+                  <q-select type="list" v-model="selectedDrug" :options="drugs"></q-select>
+                </div>
+              </div>
+            </div>
+            
+            <div class="bg-width-1of2 lg-width-2of5">
+              <div class="card">
+                <div class="card-title bg-primary text-white">Transcription Factor Selection</div>
+                <div class="card-content">
+                  <span>Please select a transcription factor.</span>
+                  <q-select type="list" v-model="selectedTf" :options="tfs"></q-select>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="card">
-          <div class="card-title bg-primary text-white">Transcription Factor Selection</div>
-          <div class="card-content">
-            <span>Please select a transcription factor.</span>
-            <q-select type="list" v-model="selectedTf" :options="tfs"></q-select>
+          <div class="row lt-bg-column justify-center gutter">
+            <div class="lt-bg-auto bg-width-1of2 lg-width-2of5 column">
+              <div class="auto">
+                <volcano-plot class="auto" :selectedDrug="selectedDrug" :selectedTf="selectedTf" :click-association-handler="clickAssociationHandler"></volcano-plot>
+              </div>
+            </div>
+            <div v-if="showSamplePlot" class="lt-bg-auto bg-width-1of2 lg-width-2of5">
+              <div class="auto">
+                <sample-plot class="auto" :drug="selectedDrug" :tf="selectedTf"></sample-plot>
+              </div>
+            </div>
           </div>
+        </div>-->
+        <!--end new layout-->
+
+
+        <div class="row">
+
+          <div class="card">
+            <div class="card-title bg-primary text-white">Drug Selection</div>
+            <div class="card-content">
+              <span>Please select a drug.</span>
+              <q-select type="list" v-model="selectedDrug" :options="drugs"></q-select>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-title bg-primary text-white">Transcription Factor Selection</div>
+            <div class="card-content">
+              <span>Please select a transcription factor.</span>
+              <q-select type="list" v-model="selectedTf" :options="tfs"></q-select>
+            </div>
+          </div>
+          
         </div>
-        <volcano-plot v-show="selectedDrug" :selectedDrug="selectedDrug" :selectedTf="selectedTf" :click-association-handler="clickAssociationHandler"></volcano-plot>
-        <sample-plot v-show="(selectedDrug !== 'all') && (selectedTf !== 'all')" :drug="selectedDrug" :tf="selectedTf"></sample-plot>
+        <volcano-plot :selectedDrug="selectedDrug" :selectedTf="selectedTf" :click-association-handler="clickAssociationHandler"></volcano-plot>
+        <sample-plot v-if="showSamplePlot" :drug="selectedDrug" :tf="selectedTf"></sample-plot>
       </div>
 
+      </div>
     </div>
+
+    <!--<div slot="footer" class="toolbar">
+      Hello, this is the footer
+    </div>-->
+
   </q-layout>
 </template>
 
@@ -51,6 +101,9 @@ export default {
     },
     tfs () {
       return store.getters.tfIndexNamePairs()
+    },
+    showSamplePlot () {
+      return (this.selectedDrug !== 'all') && (this.selectedTf !== 'all')
     }
   },
   methods: {
