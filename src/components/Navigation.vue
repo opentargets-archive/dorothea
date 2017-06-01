2e16af0d3325efda1f8350fce37b916601d2fd0d
peatroot <garethpeat@gmail.com>
2017-06-01 16:15:02 +0100

    Rename router links for investigations

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="toolbar secondary">
-    <router-link tag="button" to="/investigation/1">Investigation 1</router-link>
-    <router-link tag="button" to="/investigation/2">Investigation 2</router-link>
+    <router-link tag="button" to="/investigation/1">TF-Drug Interactions</router-link>
+    <router-link tag="button" to="/investigation/2">TF-Pharmacogenomic Marker Interactions</router-link>
     <q-toolbar-title :padding="1"></q-toolbar-title>
   </div>
 </template>
