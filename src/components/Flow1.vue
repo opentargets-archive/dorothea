85d631e2095457541c56fe7dc8dcb0fe1e89cbdd
peatroot <garethpeat@gmail.com>
2017-06-01 16:12:58 +0100

    Move footer to separate component

--- src/components/Flow1.vue
+++ src/components/Flow1.vue
@@ -44,19 +44,6 @@
         </div>
       </div>
     </div>
-    <hr>
-    <div class="row justify-start items-center gutter">
-      <div>
-        <a href="http://www.ebi.ac.uk" target="_blank">
-          <img src="../assets/ebi_195x60.png" alt="European Bioinformatics Institute" width="195px" height="60px">
-        </a>
-      </div>
-      <div>
-        <a href="https://www.targetvalidation.org/" target="_blank">
-          <img src="../assets/CTI_OT_Primary_Logo_RGB.svg" alt="Open Targets" width="200px" height="75px">
-        </a>
-      </div>
-    </div>
   </div>
 </template>
 
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,14 +2,13 @@
   <q-layout>
     <dorothea-header slot="header"></dorothea-header>
     <dorothea-navigation slot="navigation"></dorothea-navigation>
-
     <div id="main-container" class="layout-view">
       <div class="layout-padding bg-light">
         <div class="column lg-width-4of5 bg-width-4of5">
           <router-view></router-view>
+          <dorothea-footer></dorothea-footer>
         </div>
       </div>
     </div>
-    <!--<dorothea-footer></dorothea-footer>-->
   </q-layout>
 </template>
