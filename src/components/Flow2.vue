4da97d5454a45c7876b36f3d13d1b7061666e85e
peatroot <garethpeat@gmail.com>
2017-06-06 17:06:23 +0100

    Clean Flow2 component of commented code

--- src/components/Flow2.vue
+++ src/components/Flow2.vue
@@ -34,16 +34,6 @@
             </button>
           </div>
 
-          <!--<div>
-            <p class="caption">Select a GM:</p>
-            <q-select type="list" @input="changeSelectedGM" v-model="selectedGM" :options="gms"></q-select>
-          </div>-->
-
-          
-
-          <!--<div>
-            <v-client-table :data="tableData" :columns="columns" :options="options"></v-client-table>
-          </div>-->
         </div>
         <div class="card-content bg-white">
           <q-data-table :data="tableData" :config="tableConfig" :columns="tableCols"></q-data-table>
@@ -51,27 +41,15 @@
       </div>
     </div>
 
-    <!--<div class="card">
-      <div class="card-title text-primary bg-white">Query</div>
-        <div class="card-content bg-white row">
-          <div>
-            <p class="caption">Select a drug:</p>
-            <q-select type="list" @input="changeSelectedDrug" v-model="selectedDrug" :options="drugs"></q-select>
-          </div>
-        </div>
-      </div>
-    </div>-->
-
     <div v-if="selectedDrug && selectedGM && selectedTF" class="row gutter">
       <div class="width-1of2">
-        <!--<box-plot v-if="loaded" :drug="'277'" :tf="'E2F4'" :gm="'ATRX_mut'"></box-plot>-->
         <box-plot v-if="loaded" :drug="selectedDrug" :tf="selectedTF" :gm="selectedGM"></box-plot>
       </div>
       <div class="width-1of2">
-        <!--<nested-box-plot v-if="loaded" :drug="'277'" :tf="'E2F4'" :gm="'ATRX_mut'"></nested-box-plot>-->
         <nested-box-plot v-if="loaded" :drug="selectedDrug" :tf="selectedTF" :gm="selectedGM"></nested-box-plot>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -84,52 +62,37 @@ export default {
       selectedGM: null,
       selectedTF: null,
       selectedCancerType: null,
-      columns: ['drugName', 'drugId'],
-      options: {
-        filterByColumn: true,
-        listColumns: {
-          drugName: [
-            {id: 1, text: 'Dog'},
-            {id: 2, text: 'Cat'}
-          ]
-        }
-      },
       tableConfig: {
         rowHeight: '25px',
         pagination: {
           rowsPerPage: 10,
           options: [10, 25]
         }
-        // selection: 'single'
       },
       tableCols: [
         {
           label: 'Drug',
           field: 'drugName',
           width: '60px',
-          sort: true,
-          filter: true
+          sort: true
         },
         {
           label: 'Genetic Marker',
           field: 'gmId',
           width: '60px',
-          sort: true,
-          filter: true
+          sort: true
         },
         {
           label: 'TF',
           field: 'transcriptionFactor',
           width: '60px',
-          sort: true,
-          filter: true
+          sort: true
         },
         {
           label: 'Cancer Type',
           field: 'cancerType',
           width: '80px',
-          sort: true,
-          filter: true
+          sort: true
         }
       ]
     }
@@ -146,14 +109,6 @@ export default {
     },
     tfs () {
       return store.getters.flow2TFPairs(this.selectedDrug, this.selectedGM)
-    },
-    // drugSummary: function () {
-    //   let summary = store.getters.drugSummary(this.drug)
-    //   if (!summary) summary = {}
-    //   return summary
-    // },
-    drugGmPairs () {
-      return store.getters.drugGMPairData()
     },
     tableData () {
       return store.getters.flow2TableData(this.selectedDrug, this.selectedGM, this.selectedTF)
