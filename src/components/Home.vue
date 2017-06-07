3f0b2a51a58c4b4ff795e523f5dc00d3180982f0
peatroot <garethpeat@gmail.com>
2017-06-07 17:21:22 +0100

    Update home page content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,22 @@
 <template>
-<div>Home: This page is not yet implemented</div>
+  <div class="card">
+    <div class="card-content bg-white">
+<p>DoRothEA (Discriminant Regulon Expression Analysis) is a research resource that can be used to search candidate TF-drug interactions in cancer.</p>
+
+<p>Due to their location as downstream effectors of signalling pathways,
+  aberrant activities in upstream driver genes (even if not mutated)
+   will cause altered TF activities, thus proposing TFs as sensors
+    of pathway dysregulation and alternative markers. Here we study
+     the role of 127 TFs in drug sensitivity across ~1,000 cancer cell
+      lines screened with 265 anti-cancer compounds from the 
+      <a href="http://www.cancerrxgene.org/" target="_blank">GDSC</a>. In our first approach we studied how the TF activity pattern of an individual affects drug response and mined for single TF-drug statistical interactions. In our second approach we screened for TFs whose activity patterns complement or improve well-established genomic markers in the prediction of drug response.</p>
+
+<p>Please see 
+  <a href="http://biorxiv.org/content/early/2017/04/21/129478" target="_blank">Garcia-Alonso et al.</a>
+   for an explanation of the approach.</p>
+
+    </div>
+  </div>
 </template>
 
 <script>
