187b52885af98f6dc51d8e7400b2160d5c9d43ca
peatroot <garethpeat@gmail.com>
2017-06-08 10:05:35 +0100

    Add navigation link to home

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="toolbar secondary">
+    <router-link tag="button" to="/">Home</router-link>
     <router-link tag="button" to="/investigation/1">TF-Drug Interactions</router-link>
     <router-link tag="button" to="/investigation/2">TF-Pharmacogenomic Marker Interactions</router-link>
     <q-toolbar-title :padding="1"></q-toolbar-title>
