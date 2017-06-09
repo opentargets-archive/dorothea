675b48aba30069f21f30b3e8bc7ea597a6ee376f
peatroot <garethpeat@gmail.com>
2017-06-09 09:50:22 +0100

    Create card template and modify q-tabs style variables

---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card animate-fade bg-white" :padding="0">
+    <div class="card-title text-primary inverted toolbar column justify-center items-start">
+      <small><slot name="title"></slot></small>
+      <small class="text-grey-6"><slot name="description"></slot></small>
+    </div>
+
+    <q-tabs class="inverted primary" :refs="$refs" default-tab="plot-tab">
+      <q-tab name="plot-tab" class="capitalize">
+        <small>Plot</small>
+      </q-tab>
+      <q-tab name="table-tab" class="capitalize">
+        <small>Table</small>
+      </q-tab>
+    </q-tabs>
+
+    <div ref="table-tab" class="card-content">
+      <slot name="table">Table</slot>
+    </div>
+
+    <div ref="plot-tab" class="card-content">
+      <slot name="plot">Plot</slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
