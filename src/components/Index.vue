4d667d3ef28ba10f10a61ff0f1c09db91a8a6231
peatroot <garethpeat@gmail.com>
2017-06-13 10:55:12 +0100

    Show full width unless large display

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -5,7 +5,7 @@
     <div id="main-container" class="layout-view bg-light">
       <div class="full-height layout-padding">
         <div class="row justify-center">
-          <div class="width-1of1 gt-md-width-4of5">
+          <div class="width-1of1 gt-bg-width-4of5">
             <div class="column">
               <router-view></router-view>
               <dorothea-footer></dorothea-footer>
