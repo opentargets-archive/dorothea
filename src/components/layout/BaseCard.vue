3ad497cb6217984ee52493c0bb8ceb41de522ea8
peatroot <garethpeat@gmail.com>
2017-06-14 15:25:30 +0100

    Hide info by default

--- src/components/layout/BaseCard.vue
+++ src/components/layout/BaseCard.vue
@@ -1,33 +1,37 @@
 <template>
   <div class="card animate-fade bg-white">
     <div class="card-title text-primary inverted toolbar">
-      <div class="column toolbar-content">
+      <div class="toolbar-content">
         <small>{{ title }}</small>
-        <small class="text-grey-6">{{ description }}</small>
-        <!--<small>{{ title }} <button @click="toggleShowHelp"><icon name="info-circle"></icon></button></small>
-        <small v-if="showHelp" class="text-grey-6">
-          {{ description }}
-        </small>-->
+        <button class="small" @click="toggleShowHelp">
+          <icon name="info-circle"></icon>
+        </button>
       </div>
       <slot name="toolbar-button"></slot>
     </div>
+
+    <div v-if="showHelp" class="text-primary inverted toolbar">
+      <small class="text-grey-6">
+        {{ description }}
+      </small>
+    </div>
+
     <slot name="card-internals"></slot>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['title', 'description']
-  // data () {
-  //   return {
-  //     showHelp: false
-  //   }
-  // },
-  // methods: {
-  //   toggleShowHelp () {
-  //     console.log('toggle')
-  //     this.showHelp = !this.showHelp
-  //   }
-  // }
+  props: ['title', 'description'],
+  data () {
+    return {
+      showHelp: false
+    }
+  },
+  methods: {
+    toggleShowHelp () {
+      this.showHelp = !this.showHelp
+    }
+  }
 }
 </script>
