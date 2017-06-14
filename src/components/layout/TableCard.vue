e3c7afb55b7325f5441c5d4e981e0d0b20f16c02
peatroot <garethpeat@gmail.com>
2017-06-14 09:39:07 +0100

    Remove commented code from TableCard

--- src/components/layout/TableCard.vue
+++ src/components/layout/TableCard.vue
@@ -3,18 +3,8 @@
                       :description="description">
     <div slot="card-internals" class="card-content">
       <table class="q-table horizontal-delimiter compact">
-        <thead>
-          <tr v-for="key in headerKeys">
-            <th><small>{{ key }}</small></th>
-            <th class="text-right"><small>{{ tableData[key] }}</small></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="key in tableKeys">
-            <td><small>{{ key }}</small></td>
-            <td class="text-right"><small>{{ tableData[key] }}</small></td>
-          </tr>
-        </tbody>
+        <slot name="thead"></slot>
+        <slot name="tbody"></slot>
       </table>
     </div>
   </dorothea-base-card>
@@ -22,7 +12,7 @@
 
 <script>
 export default {
-  props: ['title', 'description', 'tableData', 'tableKeys', 'headerKeys']
+  props: ['title', 'description']
 }
 </script>
 
@@ -30,5 +20,6 @@ export default {
 table {
   width: 100%;
   height: 100%;
+  font-size: 80%;
 }
 </style>
