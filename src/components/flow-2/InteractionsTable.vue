858a2e3697e42a5aba6165d6ed03dc8a776d27cd
peatroot <garethpeat@gmail.com>
2017-06-15 13:06:33 +0100

    Highlight significant rows (by fdr) and format fdr/pvals

--- src/components/flow-2/InteractionsTable.vue
+++ src/components/flow-2/InteractionsTable.vue
@@ -3,15 +3,41 @@
                       description="Displaying all filtered interactions.">
 
     <div slot="card-internals" class="card-content">
-      <q-data-table :data="tableData" :config="tableConfig" :columns="tableCols"></q-data-table>
+      <small class="text-secondary">Significant interactions appear in red</small>
+      <q-data-table :data="tableData"
+                    :config="tableConfig"
+                    :columns="tableCols"
+                    @rowclick="rowClickHandler">
+        <template slot="col-drugName" scope="cell">
+          <span :class="{ 'text-secondary text-bold': significantCell(cell) }">{{  cell.data  }}</span>
+        </template>
+        <template slot="col-gm" scope="cell">
+          <span :class="{ 'text-secondary text-bold': significantCell(cell) }">{{  cell.data  }}</span>
+        </template>
+        <template slot="col-transcriptionFactor" scope="cell">
+          <span :class="{ 'text-secondary text-bold': significantCell(cell) }">{{  cell.data  }}</span>
+        </template>
+        <template slot="col-cancerType" scope="cell">
+          <span :class="{ 'text-secondary text-bold': significantCell(cell) }">{{  cell.data  }}</span>
+        </template>
+        <template slot="col-intLRTestPval" scope="cell">
+          <span :class="{ 'text-secondary text-bold': significantCell(cell) }">{{  formatter(cell.data)  }}</span>
+        </template>
+        <template slot="col-intLRTestFdr" scope="cell">
+          <span :class="{ 'text-secondary text-bold': significantCell(cell) }">{{  formatter(cell.data)  }}</span>
+        </template>
+      </q-data-table>
     </div>
   </dorothea-base-card>
 </template>
 
 <script>
+import * as d3 from 'd3'
 export default {
   data () {
     return {
+      significantCell: (cell) => cell.row.intLRTestFdr < 0.05,
+      formatter: (value) => d3.format('.3g')(value),
       tableConfig: {
         rowHeight: '25px',
         pagination: {
@@ -78,6 +104,13 @@ export default {
     tableData () {
       // TODO: Need to add cancer-type filter
       return this.$store.getters.flow2TableData(this.drugId, this.gmId, this.tfId)
+    }
+  },
+  methods: {
+    rowClickHandler (row) {
+      // note: expected feature in quasar v0.14
+      console.log('clicked a row!')
+      console.log(row)
     }
   }
 }
