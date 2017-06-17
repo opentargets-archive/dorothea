ddaf4629581793a6a7674e7693f42d76069d543f
peatroot <garethpeat@gmail.com>
2017-06-17 17:18:29 +0100

    Refactor flow 1 layout

--- src/components/Flow1.vue
+++ src/components/Flow1.vue
@@ -1,45 +1,36 @@
 <template>
-  <div class="row gutter wrap">
-
-    <div class="width-1of1">
-      <div class="card">
-        <div class="card-content bg-white text-center">
-          Single TF-drug statistical interactions between 127 TFs and 265 compound across ~1,000 cancer cell lines.
+  <div class="column">
+    <div class="row gutter wrap">
+      <div class="width-1of1">
+        <div class="card">
+          <div class="card-content bg-white text-center">
+            Single TF-drug statistical interactions between 127 TFs and 265 compound across ~1,000 cancer cell lines.
+          </div>
         </div>
       </div>
     </div>
-
-    <div class="width-1of4">
-
-      <dorothea-flow-1-filter></dorothea-flow-1-filter>
-
-        <div class="width-1of1">
-          <dorothea-sample-plot-filter v-if="showInteractionDetail"></dorothea-sample-plot-filter>
-        </div>
-
+    <div class="row gutter">
+      <div class="width-1of4">
+        <dorothea-flow-1-filter></dorothea-flow-1-filter>
       </div>
-
-      <div class="width-3of4">
-        <div class="column">
-          <div class="row gutter">
-            <div class="width-2of3">
-              <volcano-plot></volcano-plot>
-            </div>
-            <div class="width-1of3">
-              <dorothea-association-table v-if="showInteractionDetail"></dorothea-association-table>
-            </div>
-          </div>
-
-          <div class="row gutter">
-            <div class="width-2of3">
-              <sample-plot v-if="showInteractionDetail"></sample-plot>
-            </div>
-            <div class="width-1of3">
-              <dorothea-sample-table v-if="showSampleDetail"></dorothea-sample-table>
-            </div>
-          </div>
-        </div>
+      <div class="width-1of2">
+        <volcano-plot></volcano-plot>
       </div>
+      <div class="width-1of4">
+        <dorothea-association-table v-if="showInteractionDetail"></dorothea-association-table>
+      </div>
+    </div>
+    <div class="row gutter">
+      <div class="width-1of4">
+        <dorothea-sample-plot-filter v-if="showInteractionDetail"></dorothea-sample-plot-filter>
+      </div>
+      <div class="width-1of2">
+        <sample-plot v-if="showInteractionDetail"></sample-plot>
+      </div>
+      <div class="width-1of4">
+        <dorothea-sample-table v-if="showSampleDetail"></dorothea-sample-table>
+      </div>
+    </div>
   </div>
 </template>
 
