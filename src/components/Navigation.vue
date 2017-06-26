0aa68e46db5d3d0214b93a23f6b3991702133406
peatroot <garethpeat@gmail.com>
2017-06-26 18:00:13 +0100

    Hide glossary (unused)

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,6 +3,6 @@
     <q-tab route="/">Home</q-tab>
     <q-tab route="/investigation/1">TF-Drug Interactions</q-tab>
     <q-tab route="/investigation/2">TF-Pharmacogenomic Marker Interactions</q-tab>
-    <q-tab route="/glossary">Glossary</q-tab>
+    <!--<q-tab route="/glossary">Glossary</q-tab>-->
   </q-tabs>
 </template>
