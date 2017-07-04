bc3eca805ec65825348ba5f65c5982e2782ca7e3
peatroot <garethpeat@gmail.com>
2017-07-04 11:23:09 +0100

    Use capitalized text in Navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,8 +1,8 @@
 <template>
   <q-tabs class="secondary text-tertiary">
-    <q-tab route="/">Home</q-tab>
-    <q-tab route="/investigation/1">TF-Drug Interactions</q-tab>
-    <q-tab route="/investigation/2">TF-Pharmacogenomic Marker Interactions</q-tab>
-    <!--<q-tab route="/glossary">Glossary</q-tab>-->
+    <q-tab route="/" class="capitalize">Home</q-tab>
+    <q-tab route="/investigation/1" class="capitalize">TF-Drug Interactions</q-tab>
+    <q-tab route="/investigation/2" class="capitalize">TF-Pharmacogenomic Marker Interactions</q-tab>
+    <!--<q-tab route="/glossary" class="capitalize">Glossary</q-tab>-->
   </q-tabs>
 </template>
