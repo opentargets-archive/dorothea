8e370f87d7b5ba369d1b60036666e1038f6b078d
peatroot <garethpeat@gmail.com>
2017-07-13 10:57:53 +0100

    Tidy flow 2 Filter

--- src/components/flow-2/Filter.vue
+++ src/components/flow-2/Filter.vue
@@ -130,50 +130,6 @@
       <div class="group">
         <button class="capitalize primary small clear outline" @click="clickClearAllHandler">Clear All</button>
       </div>
-
-      <!--<hr/>
-      OLD
-      <div class="row justify-center items-center item">
-        <small class="width-1of3">Drug:</small>
-        <div class="width-2of3">
-          <q-select v-if="!drugId" type="list" @input="selectDrug" v-model="drugIdModel" :options="drugOptions"></q-select>
-          <small class="token" v-else>{{ drugLabel }}<i class="cursor-pointer" @click="deselectDrug()">close</i></small>
-        </div>
-      </div>
-
-      <div class="column">
-        <q-autocomplete v-if="!drugSelected" v-model="drugTerm" :static-data="staticData" :min-characters="-1" :max-results="10" @selected="selectDrugTerm">
-          <input v-model="drugTerm" class="full-width" placeholder="Type a drug name" />
-        </q-autocomplete>
-        <small v-else class="token">{{ drugSelected.value }}<i class="cursor-pointer" @click="deselectDrugTerm()">close</i></small>
-      </div>
-
-      <div class="row justify-center items-center item">
-        <small class="width-1of3">Genomic Marker:</small>
-        <div class="width-2of3">
-          <q-select v-if="!gmId" type="list" @input="selectGM" v-model="gmIdModel" :options="gmOptions"></q-select>
-          <small class="token" v-else>{{ gmLabel }}<i class="cursor-pointer" @click="deselectGM()">close</i></small>
-        </div>
-      </div>
-
-      <div class="row justify-center items-center item">
-        <small class="width-1of3">Cancer Type:</small>
-        <div class="width-2of3">
-          <q-select v-if="!ctId" type="list" @input="selectCT" v-model="ctIdModel" :options="ctOptions"></q-select>
-          <small class="token" v-else>{{ ctId }}<i class="cursor-pointer" @click="deselectCT()">close</i></small>
-        </div>
-      </div>
-
-      <div v-if="drugId && gmId" class="row justify-center items-center item">
-        <small class="width-1of3">Transcription Factor:</small>
-        <div class="width-2of3">
-          <q-select v-if="!tfId" type="list" @input="selectTF" v-model="tfIdModel" :options="tfOptions"></q-select>
-          <small class="token" v-else>{{ tfId }}<i class="cursor-pointer" @click="deselectTF()">close</i></small>
-        </div>
-      </div>
-
-      <button class="capitalize tertiary small clear outline" @click="clickClearAllHandler">Clear All</button>-->
-
     </div>
 
   </dorothea-base-card>
@@ -187,17 +143,10 @@ export default {
   components: { Multiselect },
   data () {
     return {
-      drugTerm: '',
       myDrugTerm: null,
-      // myDrugTerm: this.drugId,
       myGMTerm: null,
       myCTTerm: null,
-      myTFTerm: null,
-      drugSelected: null,
-      drugIdModel: null, // note this is not used, merely prevents vue errors
-      gmIdModel: null, // note this is not used, merely prevents vue errors
-      ctIdModel: null, // note this is not used, merely prevents vue errors
-      tfIdModel: null // note this is not used, merely prevents vue errors
+      myTFTerm: null
     }
   },
   computed: {
@@ -216,9 +165,6 @@ export default {
     tfId () {
       return store.state.route.query.filterOnTF
     },
-    // drugOptions () {
-    //   return store.state.flow2.drugOptions
-    // },
     drugAutocompleteOptions () {
       return store.state.flow2.drugAutocompleteOptions
     },
@@ -231,15 +177,6 @@ export default {
     tfAutocompleteOptions () {
       return store.state.flow2.tfAutocompleteOptions
     },
-    // gmOptions () {
-    //   return store.state.flow2.gmOptions
-    // },
-    // ctOptions () {
-    //   return store.state.flow2.ctOptions
-    // },
-    // tfOptions () {
-    //   return store.state.flow2.tfOptions
-    // },
     drugLabel () {
       let label = ''
       if (this.drugAutocompleteOptions && this.drugId) {
@@ -265,72 +202,29 @@ export default {
   },
   watch: {
     drugId: function () {
-      // this.updateGMOptions()
       this.updateGMAutocompleteOptions()
-      // this.updateCTOptions()
       this.updateCTAutocompleteOptions()
-      // this.updateTFOptions()
       this.updateTFAutocompleteOptions()
     },
     gmId: function () {
-      // this.updateDrugOptions()
       this.updateDrugAutocompleteOptions()
-      // this.updateCTOptions()
       this.updateCTAutocompleteOptions()
-      // this.updateTFOptions()
       this.updateTFAutocompleteOptions()
     },
     ctId: function () {
-      // this.updateDrugOptions()
       this.updateDrugAutocompleteOptions()
-      // this.updateGMOptions()
       this.updateGMAutocompleteOptions()
-      // this.updateTFOptions()
       this.updateTFAutocompleteOptions()
     },
     dataLoaded: function () {
-      // this.updateDrugOptions()
       this.updateDrugAutocompleteOptions()
-      // this.updateGMOptions()
       this.updateGMAutocompleteOptions()
-      // this.updateCTOptions()
       this.updateCTAutocompleteOptions()
-      // this.updateTFOptions()
       this.updateTFAutocompleteOptions()
     }
   },
   methods: {
-    // selectDrug (drugId) {
-    //   let query = {
-    //     filterOnDrug: drugId
-    //   }
-    //   if (this.gmId) query.filterOnGM = this.gmId
-    //   if (this.ctId) query.filterOnCT = this.ctId
-    //   router.push({
-    //     path: '/investigation/2',
-    //     query
-    //   })
-    // },
-    // deselectDrug () {
-    //   let query = {}
-    //   if (this.gmId) query.filterOnGM = this.gmId
-    //   if (this.ctId) query.filterOnCT = this.ctId
-    //   router.push({
-    //     path: '/investigation/2',
-    //     query
-    //   })
-    // },
-    // selectDrugTerm (item) {
-    //   this.drugSelected = item
-    // },
-    // deselectDrugTerm () {
-    //   this.drugTerm = ''
-    //   this.drugSelected = null
-    // },
     drugOptionChanged (option) {
-      // for use with vue-multiselect
-      // console.log(option)
-      // deselect
       this.myDrugTerm = null
       let query = {}
       if (option) {
@@ -344,9 +238,6 @@ export default {
       })
     },
     gmOptionChanged (option) {
-      // for use with vue-multiselect
-      // console.log(option)
-      // deselect
       this.myGMTerm = null
       let query = {}
       if (option) {
@@ -360,9 +251,6 @@ export default {
       })
     },
     ctOptionChanged (option) {
-      // for use with vue-multiselect
-      // console.log(option)
-      // deselect
       this.myCTTerm = null
       let query = {}
       if (option) {
@@ -376,9 +264,6 @@ export default {
       })
     },
     tfOptionChanged (option) {
-      // for use with vue-multiselect
-      // console.log(option)
-      // deselect
       this.myTFTerm = null
       let query = {}
       if (option) {
@@ -392,80 +277,12 @@ export default {
         query
       })
     },
-    // selectGM (gmId) {
-    //   let query = {
-    //     filterOnGM: gmId
-    //   }
-    //   if (this.drugId) query.filterOnDrug = this.drugId
-    //   if (this.ctId) query.filterOnCT = this.ctId
-    //   router.push({
-    //     path: '/investigation/2',
-    //     query
-    //   })
-    // },
-    // deselectGM () {
-    //   let query = {}
-    //   if (this.drugId) query.filterOnDrug = this.drugId
-    //   if (this.ctId) query.filterOnCT = this.ctId
-    //   router.push({
-    //     path: '/investigation/2',
-    //     query
-    //   })
-    // },
-    // selectCT (ctId) {
-    //   let query = {
-    //     filterOnCT: ctId
-    //   }
-    //   if (this.drugId) query.filterOnDrug = this.drugId
-    //   if (this.gmId) query.filterOnGM = this.gmId
-    //   router.push({
-    //     path: '/investigation/2',
-    //     query
-    //   })
-    // },
-    // deselectCT () {
-    //   let query = {}
-    //   if (this.drugId) query.filterOnDrug = this.drugId
-    //   if (this.gmId) query.filterOnGM = this.gmId
-    //   router.push({
-    //     path: '/investigation/2',
-    //     query
-    //   })
-    // },
-    // selectTF (tfId) {
-    //   let query = {
-    //     filterOnTF: tfId
-    //   }
-    //   if (this.drugId) query.filterOnDrug = this.drugId
-    //   if (this.gmId) query.filterOnGM = this.gmId
-    //   if (this.ctId) query.filterOnCT = this.ctId
-    //   router.push({
-    //     path: '/investigation/2',
-    //     query
-    //   })
-    // },
-    // deselectTF () {
-    //   let query = {}
-    //   if (this.drugId) query.filterOnDrug = this.drugId
-    //   if (this.gmId) query.filterOnGM = this.gmId
-    //   if (this.ctId) query.filterOnCT = this.ctId
-    //   router.push({
-    //     path: '/investigation/2',
-    //     query
-    //   })
-    // },
     clickClearAllHandler () {
       router.push({
         path: '/investigation/2',
         query: {}
       })
     },
-    // updateDrugOptions () {
-    //   store.dispatch('flow2/updateDrugOptions', {
-    //     gmId: this.gmId,
-    //     ctId: this.ctId
-    //   })
-    // },
     updateDrugAutocompleteOptions () {
       store.dispatch('flow2/updateDrugAutocompleteOptions', {
         gmId: this.gmId,
@@ -491,38 +308,15 @@ export default {
         ctId: this.ctId
       })
     },
-    // updateGMOptions () {
-    //   store.dispatch('flow2/updateGMOptions', {
-    //     drugId: this.drugId,
-    //     ctId: this.ctId
-    //   })
-    // },
-    // updateCTOptions () {
-    //   store.dispatch('flow2/updateCTOptions', {
-    //     drugId: this.drugId,
-    //     gmId: this.gmId
-    //   })
-    // },
-    // updateTFOptions () {
-    //   store.dispatch('flow2/updateTFOptions', {
-    //     drugId: this.drugId,
-    //     gmId: this.gmId,
-    //     ctId: this.ctId
-    //   })
-    // },
     drugNameWithSynonym (option) {
       if (option.label === option.value) return option.label
       return option.label + ' (synonym of ' + option.value + ')'
     }
   },
   mounted () {
-    // this.updateDrugOptions()
     this.updateDrugAutocompleteOptions()
-    // this.updateGMOptions()
     this.updateGMAutocompleteOptions()
-    // this.updateCTOptions()
     this.updateCTAutocompleteOptions()
-    // this.updateTFOptions()
     this.updateTFAutocompleteOptions()
   }
 }
