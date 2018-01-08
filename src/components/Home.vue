f4fea5f7619a06986d993e4871f42f1ecc0868a8
peatroot <garethpeat@gmail.com>
2018-01-08 11:41:04 +0000

    Remove the under development sign

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,15 +2,8 @@
   <div>
     <div v-if="dataLoaded" class="column">
       <div class="card">
-        <div class="card-content bg-warning">
-          <icon name="warning"></icon>
-          Please note that this site is currently under active development.
-        </div>
-      </div>
-
-      <div class="card">
         <div class="card-content bg-white">
-          <p>DoRothEA (Discriminant Regulon Expression Analysis) is a research 
+          <p>DoRothEA (Discriminant Regulon Expression Analysis) is a research
             resource that can be used to search candidate TF-drug interactions in cancer.</p>
 
           <p>Due to their location as downstream effectors of signalling pathways,
@@ -18,14 +11,14 @@
             will cause altered TF activities, thus proposing TFs as sensors
               of pathway dysregulation and alternative markers. Here we study
               the role of 127 TFs in drug sensitivity across ~1,000 cancer cell
-                lines screened with 265 anti-cancer compounds from the 
-                <a href="http://www.cancerrxgene.org/" target="_blank">GDSC</a>. In our 
-                first approach we studied how the TF activity pattern of an individual 
-                affects drug response and mined for single TF-drug statistical interactions. 
-                In our second approach we screened for TFs whose activity patterns complement 
+                lines screened with 265 anti-cancer compounds from the
+                <a href="http://www.cancerrxgene.org/" target="_blank">GDSC</a>. In our
+                first approach we studied how the TF activity pattern of an individual
+                affects drug response and mined for single TF-drug statistical interactions.
+                In our second approach we screened for TFs whose activity patterns complement
                 or improve well-established genomic markers in the prediction of drug response.</p>
 
-          <p>Please see 
+          <p>Please see
             <a href="http://biorxiv.org/content/early/2017/04/21/129478" target="_blank">Garcia-Alonso et al.</a>
             for an explanation of the approach.</p>
         </div>
